7d54cfb0f1adb5eb7eacc5d94ac0eee2b787a503
Gie Spaepen <gie.spaepan@zna.be>
2018-04-25 12:16:27 +0200

    #2 Version 0.1.9

--- flow-typed/index.flow.js
+++ flow-typed/index.flow.js
@@ -14,25 +14,25 @@ declare export type ReduxSagaMock = {
 /**
  * This factory creates a mocked store object object and an instance
  * of the saga middleware.
- * @param  
- * @param  
+ * @param  undefined
+ * @param  undefined
  */
 declare export function mockStoreFactory(rootReducer: any, store: Store<any>, state?: any): ReduxSagaMock
 /**
  * Sagas always start with a root saga where all the other sagas are forked.
- * @param  
+ * @param  undefined
  */
 declare export function createMockRootSaga(...sagas: any[]): any
 /**
  * Run the sagas against the configured mock saga middleware. Return the done promise to await
  * in your tests.
- * @param mock 
- * @param sagas 
+ * @param  undefined
+ * @param  undefined
  */
 declare export function runSagaMock(mock: ReduxSagaMock, ...sagas: any[]): Promise<any >
     /**
      * Stop a saga mock by dispatching the END signal. Don't forget to await the promise returned
      * in runSagaMock.
-     * @param mock 
+     * @param  undefined
      */
     declare export function stopSagaMock(mock: ReduxSagaMock): void
